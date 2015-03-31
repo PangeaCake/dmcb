2e8cf007996d060672e995e5f3fa7c491c860210
PangeaCake <PangeaCake@users.noreply.github.com>
2015-03-30 17:46:45 -0700

    Spelling mistakes..

--- dmcb/views.py
+++ dmcb/views.py
@@ -5,18 +5,18 @@ from dmcb import app, cache, generator;
 
 # The views:
 
-@app.route('/<name>/<adress>/banner.png')
-@app.route('/1.7/<name>/<adress>/banner.png')
-def name_adress(name, adress):
-    return send_file(wrapper(name, adress), mimetype="image/png",
-                     as_attachment=False)
+@app.route('/<name>/<address>/banner.png')
+@app.route('/1.7/<name>/<address>/banner.png')
+def name_address(name, address):
+    return send_file(wrapper(name, adress),
+                     mimetype="image/png", as_attachment=False)
 
-@app.route('/<name>/<adress>/<int:port>/banner.png')
-@app.route('/1.7/<name>/<adress>/<int:port>/banner.png')
-def name_adress_port(name, adress, port):
-    return send_file(wrapper(name, adress, port=port),
+@app.route('/<name>/<address>/<int:port>/banner.png')
+@app.route('/1.7/<name>/<address>/<int:port>/banner.png')
+def name_address_port(name, address, port):
+    return send_file(wrapper(name, address, port=port),
                      mimetype="image/png", as_attachment=False)
 
 @cache.memoize(timeout=app.config['TIMEOUT'])
-def wrapper(name, adress, port = 25565):
-    return generator.banner(name, adress, port=port)
+def wrapper(name, address, port=25565):
+    return generator.banner(name, address, port=port)
